e0c52a615c836156c5dbbfcedd2f7b76aa0e4a64
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-14 23:15:49 +0530

    [Durgesh Kushwaha] Add - added the test code for 'exception propagation in method' problem✨

---
+++ Day4-Exceptions/src/test/java/exceptions/exception_propagation_in_methods/ExceptionPropagationTest.java
@@ -0,0 +1,16 @@
+package exceptions.exception_propagation_in_methods;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ExceptionPropagationTest {
+
+    ExceptionPropagation excep = new ExceptionPropagation(10);
+
+    @Test
+    void Test1(){
+        assertThrows(ArithmeticException.class,()-> excep.method2());
+        System.out.println("Exception occurs in method1 but handled in method2");
+    }
+}
