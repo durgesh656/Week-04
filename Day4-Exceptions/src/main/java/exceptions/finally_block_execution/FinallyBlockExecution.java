ee94400157bf96b10f420a2fe813f2a7f581af7e
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-14 23:18:12 +0530

    [Durgesh Kushwaha] Add - added the solution of 'finally block execution' problem✨

---
+++ Day4-Exceptions/src/main/java/exceptions/finally_block_execution/FinallyBlockExecution.java
@@ -0,0 +1,9 @@
+package exceptions.finally_block_execution;
+
+
+public class FinallyBlockExecution {
+    public static int resultCalculate(int num, int num1) {
+
+           return num/num1;
+    }
+}
