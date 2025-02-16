cafa67c417015ba3cf6e576cd9c410f4e777ffd4
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-17 00:29:38 +0530

    [Durgesh Kushwaha] Add - added the solution of 'extract links from a web page' problem✨

---
+++ Day5-Regex and Junit/src/main/java/regex_and_junit/regex_problems/extract_links_from_a_web_page/Main.java
@@ -0,0 +1,24 @@
+package regex_and_junit.regex_problems.extract_links_from_a_web_page;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //use try and catch to handle exception if occurs
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+
+            //get user input
+            System.out.print("Enter your website url: ");
+            String text = br.readLine().trim();
+
+            System.out.println(ExtractLink.extractLink(text));
+
+        }catch (IOException e){
+            System.out.println("Exception caught "+e.getMessage());
+        }
+    }
+}
