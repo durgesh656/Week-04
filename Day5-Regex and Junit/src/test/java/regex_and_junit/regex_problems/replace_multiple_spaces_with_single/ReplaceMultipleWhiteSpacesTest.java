d84f46e8b1231c76ad0d9dd0f9526ffbb8704bbc
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-18 11:24:41 +0530

    [Durgesh kushwaha] Add - added the test code for 'replace multiple spaces with single' problem✨

---
+++ Day5-Regex and Junit/src/test/java/regex_and_junit/regex_problems/replace_multiple_spaces_with_single/ReplaceMultipleWhiteSpacesTest.java
@@ -0,0 +1,14 @@
+package regex_and_junit.regex_problems.replace_multiple_spaces_with_single;
+
+import regex_and_junit.regex_problems.replace_multiple_space_with_single.ReplaceMultipleSpaces;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReplaceMultipleWhiteSpacesTest {
+    @Test
+    void removeSpacesCase(){
+        String text = "hey      hero      how   was your    day";
+        assertEquals("hey hero how was your day", ReplaceMultipleSpaces.replaceMultipleSpaces(text));
+    }
+}
