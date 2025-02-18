27cbc99a047b950f37ff1156546770f4735158c3
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-18 11:28:52 +0530

    [Durgesh kushwaha] Add - added the test code for 'validate ip address' problem✨

---
+++ Day5-Regex and Junit/src/test/java/regex_and_junit/regex_problems/validate_ip_address/ValidateIpAddressTest.java
@@ -0,0 +1,13 @@
+package regex_and_junit.regex_problems.validate_ip_address;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ValidateIpAddressTest {
+    @Test
+    void checkIp(){
+        String ip = "255.195.245.196";
+        assertTrue(ValidateIpAddress.validateIpAddress(ip));
+    }
+}
