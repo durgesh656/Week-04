bc0c563c5d231fced5da316272321a3b8c2ef512
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-19 11:59:32 +0530

    [Durgesh Kushwaha] Add - added the solution of 'role based access control' problem✨

---
+++ Day6-Annotations and Reflection/src/main/java/annotations/advance_level/role_based_access_control/User.java
@@ -0,0 +1,14 @@
+package annotations.advance_level.role_based_access_control;
+
+public class User {
+
+    private String role;
+
+    public User(String role){
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
