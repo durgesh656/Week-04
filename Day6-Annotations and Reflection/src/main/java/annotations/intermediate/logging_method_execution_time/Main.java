fd0d015a8d2c4d687e9d3aef9d253d21317ad8b8
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-19 12:06:53 +0530

    [Durgesh Kushwaha] Add - added the solution of 'logging method execution time' problem✨

---
+++ Day6-Annotations and Reflection/src/main/java/annotations/intermediate/logging_method_execution_time/Main.java
@@ -0,0 +1,10 @@
+package annotations.intermediate.logging_method_execution_time;
+
+public class Main {
+    public static void main(String[] args) {
+        TaskManager manager = new TaskManager();
+        ExecutionTimeLogger.logExecutionTime(manager, "processTask");
+        ExecutionTimeLogger.logExecutionTime(manager, "fastTask");
+        ExecutionTimeLogger.logExecutionTime(manager, "withoutAnnotation");
+    }
+}
