159cb8fa19fe99ef9bc83a150b82e2a187c23302
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-20 00:23:40 +0530

    [Durgesh Kushwaha] Add - added the solution of 'insurance policy management system' problem✨

---
+++ Day2-Collections/src/main/java/collections/insurance_policy_management_system/Main.java
@@ -0,0 +1,20 @@
+package collections.insurance_policy_management_system;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        InsurancePolicyManagement<InsurancePolicy> management = new InsurancePolicyManagement<>();
+
+        management.addPolicy(new InsurancePolicy(1, "Durgesh", LocalDate.now().plusDays(15), "Health", 500.0));
+        management.addPolicy(new InsurancePolicy(2, "Om Tiwari", LocalDate.now().plusDays(45), "Auto", 750.0));
+        management.addPolicy(new InsurancePolicy(3, "Bipin", LocalDate.now().plusDays(10), "Home", 1000.0));
+        management.addPolicy(new InsurancePolicy(1, "Harshveer", LocalDate.now().plusDays(15), "Health", 500.0)); // Duplicate
+
+        management.displayAllPolicies();
+        management.displayExpiringSoon();
+        management.displayByCoverageType("Health");
+        management.findDuplicatePolicies();
+    }
+
+}
