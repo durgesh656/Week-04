cf5e4fda544fe5c4a6154c9413fe02b9f87467ad
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-19 12:08:49 +0530

    [Durgesh Kushwaha] Add - added the solution of 'suppress unchecked warnings' problem✨

---
+++ Day6-Annotations and Reflection/src/main/java/annotations/suppress_unchecked_warnings/Main.java
@@ -0,0 +1,8 @@
+package annotations.suppress_unchecked_warnings;
+
+public class Main {
+    public static void main(String[] args) {
+        //invoking method addElements()
+        SupressArrayListWarnings.addElements();
+    }
+}
---
+++ Day6-Annotations and Reflection/src/main/java/annotations/suppress_unchecked_warnings/SupressArrayListWarnings.java
@@ -0,0 +1,18 @@
+package annotations.suppress_unchecked_warnings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SupressArrayListWarnings {
+    @SuppressWarnings("unchecked")
+    public static void addElements(){
+        List list = new ArrayList();
+
+        list.add(11);
+        list.add("Hello");
+        list.add(true);
+        list.add(14.9);
+
+        System.out.println("List contains: "+ list);
+    }
+}
