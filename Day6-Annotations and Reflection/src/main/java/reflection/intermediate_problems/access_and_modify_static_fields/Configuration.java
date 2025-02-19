6fa899f23a605260b1dc6dd52e0f133f6ba11415
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-19 12:23:41 +0530

    [Durgesh Kushwaha] Add - added the solution of 'access and modify static fields' problem✨

---
+++ Day6-Annotations and Reflection/src/main/java/reflection/intermediate_problems/access_and_modify_static_fields/Configuration.java
@@ -0,0 +1,11 @@
+package reflection.intermediate_problems.access_and_modify_static_fields;
+
+public class Configuration {
+    // Private static field
+    private static String API_KEY = "DEFAULT_KEY";
+
+    // Method to display API key
+    public static void displayAPIKey() {
+        System.out.println("API_KEY: " + API_KEY);
+    }
+}
